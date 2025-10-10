8c22413817325cab9cd93c0f369ea5c115076272
Ariq Adlan Hadiyan <ariqhadiyan25@gmail.com>
2025-10-10 11:37:28 +0700

    vite env

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,26 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.jsx',
+
+        'resources/assets/vendor/scss/theme-default.scss',
+        'resources/assets/vendor/libs/apex-charts/apex-charts.scss',
+        'resources/assets/vendor/libs/apex-charts/apexcharts.js',
+
+        // 'resources/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js',
+        // 'resources/assets/vendor/libs/boxicons/css/boxicons.css',
+      ],
       refresh: true,
     }),
     react(),
   ],
+
+  // Biar path relatif tetap aman di Railway
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    emptyOutDir: true,
+  },
 });
