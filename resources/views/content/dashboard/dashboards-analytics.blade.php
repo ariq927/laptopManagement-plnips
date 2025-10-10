968d53d31bf33f51961490c435f9bbd89eb54553
Ariq Adlan Hadiyan <ariqhadiyan25@gmail.com>
2025-10-10 10:53:31 +0700

    remove apex

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Dashboard - Laptop Management PLN IPS')
 
 @section('vendor-style')
-@vite('resources/assets/vendor/libs/apex-charts/apex-charts.scss')
+<!-- @vite('resources/assets/vendor/libs/apex-charts/apex-charts.scss') -->
 @endsection
 
 @section('vendor-script')
-@vite('resources/assets/vendor/libs/apex-charts/apexcharts.js')
+<!-- @vite('resources/assets/vendor/libs/apex-charts/apexcharts.js') -->
 @endsection
 
 @section('page-style')
